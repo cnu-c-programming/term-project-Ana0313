2bee39f661f33d4908b7cfe65d479cd9af51ae49
Ana0313 <rokafosh2@gmail.com>
2026-06-18 18:00:44 +0900

    Update main.c

--- project/main.c
+++ project/main.c
@@ -23,6 +23,34 @@ void start_shell(const char *csv_path, Student **head){
     }
 }
 
+void run_shell(const char *csv_path){
+    Student *head=NULL;
+    start_shell(csv_path, &head);
+
+    char input[256];
+    while(1){
+#ifdef ADMIN_MODE
+        printf("admin> ");
+#else
+        printf("client> ");
+#endif
+        if(fgets(input, sizeof(input), stdin)==NULL) break;
+        
+        input[strcspn(input, "\r\n")]='\0';
+        if(strlen(input)==0 || input[0]=='#') continue;
+
+        ShellResult error_type=run_command(input, &head, csv_path);
+
+        if(error_type==SHELL_EXIT){
+            printf("Goodbye.\n");
+            break;
+        }else if(error_type!=SHELL_OK){
+            print_error(error_type);
+        }
+    }
+    free_all_students(&head);
+}
+
 int main(int argc, char *argv[]){
     const char *csv_path=NULL;
     const char *cmd_file=NULL;
@@ -52,6 +80,8 @@ int main(int argc, char *argv[]){
 #endif
         return 1;
     }
+
+    run_shell(csv_path);
 
     return 0;
 }
